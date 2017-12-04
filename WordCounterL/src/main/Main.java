36723abd240df944d37328fea27ed89766d70060
khmu97 <34014734+khmu97@users.noreply.github.com>
2017-12-04 18:37:00 -0500

    reporter still there

--- WordCounterL/src/main/Main.java
+++ WordCounterL/src/main/Main.java
@@ -1,17 +1,13 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
 
-import components.map.Map;
-import components.map.Map.Pair;
-import components.sequence.Sequence;
-import components.sequence.Sequence1L;
-import components.simplereader.SimpleReader;
-import components.simplereader.SimpleReader1L;
-import components.simplewriter.SimpleWriter;
-import components.simplewriter.SimpleWriter1L;
-import components.sortingmachine.SortingMachine;
-import components.sortingmachine.SortingMachine1L;
 import components.utilities.Reporter;
 
 /**
@@ -23,146 +19,127 @@ import components.utilities.Reporter;
  */
 public final class Main {
 
-    /**
-     * Private default constructor prevents instantiation.
-     */
-    private Main() {
-    }
+	/**
+	 * Private default constructor prevents instantiation.
+	 */
+	private Main() {
+	}
 
-    /**
-     * Main function manages user input and dispatches jobs.
-     *
-     * @param args
-     *            - command line arguments.
-     */
-    public static void main(String[] args) {
-        //open input and output streams
-        SimpleReader in = new SimpleReader1L();
-        SimpleWriter out = new SimpleWriter1L();
+	/**
+	 * Main function manages user input and dispatches jobs.
+	 *
+	 * @param args
+	 *            - command line arguments.
+	 */
+	public static void main(String[] args) throws IOException {
+		// open input and output streams
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        //Get input from user
-        String filename;
-        out.println("Enter name of input file: ");
-        filename = in.nextLine();
+		// Get input from user
+		String filename;
+		System.out.println("Enter name of input file: ");
+		filename = in.readLine();
 
-        //get number of words from user
-        int numWords;
-        out.println("How many words in outputed file: ");
-        numWords = in.nextInteger();
-        Reporter.assertElseFatalError(numWords >= 1,
-                "Number of words must be greater than 0");
+		// get number of words from user
+		int numWords;
+		System.out.println("How many words in outputed file: ");
+		numWords = Integer.parseInt(in.readLine());
+		Reporter.assertElseFatalError(numWords >= 1, "Number of words must be greater than 0");
 
-        //Read from file to list
-        out.print("Beginning reading from file...");
-        Sequence<String> pRawWordList = CWordCounter
-                .separateWordsFromFile(filename, " \t\n\r,-.!?[]';:/()");
-        Reporter.assertElseFatalError(pRawWordList.length() > 0,
-                "Text file must not be empty of words");
+		// Read from file to list
+		System.out.print("Beginning reading from file...");
 
-        //Sort the words
-        Map<String, Integer> pWordCounts = CWordCounter
-                .wordCountMap(pRawWordList);
-        Sequence<String> pWordList = sortWordListAlphabeticalMostPop(
-                pWordCounts, numWords);
-        out.println("Finished!");
+		ArrayList<String> pRawWordList = CWordCounter.separateWordsFromFile(filename, " \t\n\r,-.!?[]';:/()");
+		Reporter.assertElseFatalError(pRawWordList.length() > 0, "Text file must not be empty of words");
 
-        //Calculate results & write to file
-        // Top 100 words in data/importance.txt
-        String title = "Top " + numWords + " words in " + filename;
-        out.println("Enter name of output file: ");
-        filename = in.nextLine();
-        CWordCounter.outputToHtml(pWordList, pWordCounts, filename, title);
+		// Sort the words
+		Map<String, Integer> pWordCounts = CWordCounter.wordCountMap(pRawWordList);
+		ArrayList<String> pWordList = sortWordListAlphabeticalMostPop(pWordCounts, numWords);
+		System.out.println("Finished!");
 
-        out.println("Finished writing to " + filename);
+		// Calculate results & write to file
+		// Top 100 words in data/importance.txt
+		String title = "Top " + numWords + " words in " + filename;
+		System.out.println("Enter name of output file: ");
+		filename = in.readLine();
+		CWordCounter.outputToHtml(pWordList, pWordCounts, filename, title);
 
-        //close input and output streams
-        in.close();
-        out.close();
-    }
+		System.out.println("Finished writing to " + filename);
 
-    /**
-     * Given a list of words, puts it into alphabetical order, whic A, B C etc.
-     * coming at the beginning of the sequence
-     *
-     * @param pWordList
-     *            - the sequence to sort
-     */
-    public static void sortWordList(Sequence<String> pWordList) {
-        //just use the sorting machine... ughh
-        Comparator<String> pOrder = String.CASE_INSENSITIVE_ORDER;
-        SortingMachine<String> pSorter = new SortingMachine1L<String>(pOrder);
+		// close input and output streams
+		in.close();
+		System.out.close();
 
-        //dump it into the black box
-        while (pWordList.length() > 0) {
-            pSorter.add(pWordList.remove(0));
-        }
+	}
 
-        pSorter.changeToExtractionMode();
+	/**
+	 * Given a list of words, puts it into alphabetical order, whic A, B C etc.
+	 * coming at the beginning of the sequence
+	 *
+	 * @param pWordList
+	 *            - the sequence to sort
+	 */
+	public static void sortWordList(ArrayList<String> pWordList) {
+		// just use the sorting machine... ughh
+		Comparator<String> pOrder = String.CASE_INSENSITIVE_ORDER;
+		pWordList.sort(pOrder);
+	}
 
-        //now take it all back out
-        while (pSorter.size() > 0) {
-            pWordList.add(pWordList.length(), pSorter.removeFirst());
-        }
-    }
+	/**
+	 * Compares the integer values of a Pair<String, Integer> .
+	 *
+	 * @author Michael Trunk
+	 */
+	static class CPopularityOrder implements Comparator<Map.Entry<String, Integer>> {
 
-    /**
-     * Compares the integer values of a Pair<String, Integer> .
-     *
-     * @author Michael Trunk
-     */
-    static class CPopularityOrder
-            implements Comparator<Map.Pair<String, Integer>> {
+		@Override
+		public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+			if (o1.getKey().equals(o2.getKey())) {
+				return 0;
+			} else {
+				return o1.getValue() < o2.getValue() ? 1 : -1;
+			}
+		}
 
-        @Override
-        public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
-            if (o1.key().equals(o2.key())) {
-                return 0;
-            } else {
-                return o1.value() < o2.value() ? 1 : -1;
-            }
-        }
+	}
 
-    }
+	/**
+	 * Global ordering object.
+	 */
+	public static final CPopularityOrder POPULARITY_ORDER = new CPopularityOrder();
 
-    /**
-     * Global ordering object.
-     */
-    public static final CPopularityOrder POPULARITY_ORDER = new CPopularityOrder();
+	/**
+	 * Given a map, returns an alphabetically ordered list of the given length
+	 * whose elements are the most common strings appearing in the given map.
+	 *
+	 * @param pWordCounts
+	 *            - map of words to their counts
+	 * @param amountFirstWords
+	 *            - the amount of most popular words to put into the returned
+	 *            Sequence
+	 * @return - the sequence of most common words
+	 */
+	public static ArrayList<String> sortWordListAlphabeticalMostPop(Map<String, Integer> pWordCounts,
+			int amountFirstWords) {
+		ArrayList<Map.Entry<String, Integer>> mapList = new ArrayList<>();
+		// dump it into the black box
+		Set<Map.Entry<String, Integer>> entries = pWordCounts.entrySet();
+		for (Map.Entry<String, Integer> currPair : entries) {
+			mapList.add(currPair);
+		}
+		mapList.sort(POPULARITY_ORDER);
+		ArrayList<String> pWordList = new ArrayList<>();
+		// now take it all back out
+		int i = 0;
+		while (mapList.size() > 0 & i < amountFirstWords) {
+			pWordList.add(pWordList.size(), mapList.remove(0).getKey());
+			i++;
+		}
 
-    /**
-     * Given a map, returns an alphabetically ordered list of the given length
-     * whose elements are the most common strings appearing in the given map.
-     *
-     * @param pWordCounts
-     *            - map of words to their counts
-     * @param amountFirstWords
-     *            - the amount of most popular words to put into the returned
-     *            Sequence
-     * @return - the sequence of most common words
-     */
-    public static Sequence<String> sortWordListAlphabeticalMostPop(
-            Map<String, Integer> pWordCounts, int amountFirstWords) {
+		// now sort our result alphabetically
+		sortWordList(pWordList);
 
-        SortingMachine<Map.Pair<String, Integer>> pSorter = new SortingMachine1L<>(
-                POPULARITY_ORDER);
-
-        //dump it into the black box
-        for (Map.Pair<String, Integer> currPair : pWordCounts) {
-            pSorter.add(currPair);
-        }
-        pSorter.changeToExtractionMode();
-        Sequence<String> pWordList = new Sequence1L<>();
-        //now take it all back out
-        int i = 0;
-        while (pSorter.size() > 0 & i < amountFirstWords) {
-            pWordList.add(pWordList.length(), pSorter.removeFirst().key());
-            i++;
-        }
-
-        //now sort our result alphabetically
-        sortWordList(pWordList);
-
-        return pWordList;
-    }
+		return pWordList;
+	}
 
 }
